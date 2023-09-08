a100db557cbff6a578ec524589d8ae69399d198d
Kilo K7ILO <44325797+k7ilo@users.noreply.github.com>
2023-09-07 17:03:13 -0700

    Uploaded scripts

---
+++ fdmr-mon_install.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+# K7ILO's FDMR-Monitor Installer V1
+#
+#Lets begin-------------------------------------------------------------------------------------------------
+if [ "$EUID" -ne 0 ];
+then
+echo ""
+echo " You Must be root to run this script!! "
+exit 1
+fi
+if [ ! -e "/etc/debian_version" ]
+then
+echo ""
+echo " This script has been tested on NEWLY INSTALLED Debian 10 and 11 64-bit repo's only. "
+echo " It is assumed you are doing the same. "
+exit 0
+fi
+
+VERSION=$(sed 's/\..*//' /etc/debian_version)
+INSDIR=/opt
+HBMONDIR=$INSDIR/FDMR-Monitor
+HBMONREPO=https://github.com/yuvelq/FDMR-Monitor.git
+DEP="curl python3 python3-dev python3-pip libffi-dev libssl-dev sed apache2 php ca-certificates"
+
+if [ $VERSION = 10 ];
+then
+echo " Debian 10 (Buster) is detected "
+echo ""
+echo "----------------------------------------------------------------------"
+echo " Downloading and installing the required software & dependencies .... "
+echo "----------------------------------------------------------------------"
+sleep 2
+apt update && apt install -y $DEP
+apt autoremove && apt autoclean
+echo ""
+echo " Installing the Rust compilter.  Select default (1) when asked .... "
+sleep 3
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+sleep 2
+
+elif [ $VERSION = 11 ];
+then
+echo " Debian 11 (Bullseye) is detected "
+echo ""
+echo "----------------------------------------------------------------------"
+echo " Downloading and installing the required software & dependencies .... "
+echo "----------------------------------------------------------------------"
+sleep 2
+apt update && apt install -y $DEP
+apt autoremove && apt autoclean
+echo ""
+echo " Installing the Rust compilter.  Select default (1) when asked .... "
+sleep 3
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+sleep 2
+
+else
+echo "------------------------------------------------------------------------------------------"
+echo " Operating system not supported! Please check your configuration or upgrade. Exiting .... "
+echo "------------------------------------------------------------------------------------------"
+exit 0
+fi
+
+echo ""
+echo "----------------------------------------------------------------------------"
+echo " Now lets download and configure the FDMR-Monitor Dashboard repository .... "
+echo "----------------------------------------------------------------------------"
+sleep 2
+git clone $HBMONREPO
+cd $HBMONDIR
+if [ -e monitor.py ]
+then
+echo ""
+echo "--------------------------------------------------------------------"
+echo " I can see we are in the FDMR-Monitor director. Let's continue .... "
+echo "--------------------------------------------------------------------"
+else
+echo "------------------------------------------------------------------------------------------------ "
+echo " It doesn't seem like you're in the FDMR-Monitor directory. Please see if you are.  Exiting .... "
+echo "------------------------------------------------------------------------------------------------ "
+exit 0
+fi
+
+echo "-----------------------------------------------------------------------------"
+echo " Downloading and installing dependencies from the requirements.txt file .... "
+echo "-----------------------------------------------------------------------------"
+pip3 install -r requirements.txt
+sleep 2
+echo "--------------------------------------"
+echo " Creating the configuratoin file .... "
+echo "--------------------------------------"
+cp fdmr-mon_SAMPLE.cfg fdmr-mon.cfg
+chmod 644 fdmr-mon.cfg
+sleep 2
+
+echo ""
+echo "---------------------------------------------------"
+echo " Backing up Apache2's default index.html file .... "
+echo "---------------------------------------------------"
+cd /var/www/html/
+if [ -e index.html ]
+then
+mv /var/www/html/index.html /var/www/html/index.html.bak
+echo ""
+echo " Done .... "
+else
+echo ""
+echo "-------------------------------------------------------------------"
+echo " No Apache2 default index.html file found.  Continuing anyway .... "
+echo "-------------------------------------------------------------------"
+fi
+
+echo ""
+echo "------------------------------------------------------"
+echo " Installing the FDMR-Monitor Dashboard web files .... "
+echo "------------------------------------------------------"
+cp -a $HBMONDIR/html/. /var/www/html/
+if [ -e info.php ]
+then
+echo ""
+echo "------------------------------------------------------"
+echo " It looks like the web files installed correctly .... "
+echo "------------------------------------------------------"
+else
+echo ""
+echo "----------------------------------------------------------------------------------------------"
+echo " OOPS! I don't see the web files. Please check your configuration and try again. Exiting .... "
+echo "----------------------------------------------------------------------------------------------"
+exit 0
+fi
+
+echo ""
+echo "---------------------------------------------------------------"
+echo " Creating, enabling and starting the FDMR-Monitor service .... "
+echo "---------------------------------------------------------------"
+cp $HBMONDIR/utils/systemd/fdmr_mon.service /lib/systemd/system/
+systemctl enable fdmr_mon
+systemctl start fdmr_mon
+echo " Done .... "
+sleep 2
+
+clear
+echo " And it looks like we're done with installing FDMR-Monitor  "
+echo " Check and see if it's running by typing: systemctl status fdmr_mon  "
+echo " Access the Dashboard by going to the Dashboards URL at http://<server_address> "
+echo " 73 "
---
+++ ufw-firewall_install.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# K7ILO's ufw firewall Installer and configurer
+
+if [ "$EUID" -ne 0 ];
+then
+  echo ""
+  echo " You must be root to run this script!! "
+  exit 1
+fi
+
+if [ ! -e "/etc/debian_version" ]
+then
+  echo ""
+  echo "This script has been tested on Debian 10 and 11 64-bit repo's only."
+  exit 0
+fi
+
+# Global Parameters -------------------------------------------------------------------------------------------------
+VERSION=$(sed 's/\..*//' /etc/debian_version)
+DEP="ufw"
+sshport=7736
+
+# Lets begin --------------------------------------------------------------------------------------------------------
+if [ $VERSION = 10 ];
+then
+	echo " Debian 10 (Buster) is detected "
+	sleep 2
+	clear
+	if pgrep -x ufw >/dev/null
+	then
+		echo "--------------------------------------------------------------------------"
+		echo " ufw is already installed.  The ports just need to be opened.  Moving on! "
+		echo "--------------------------------------------------------------------------"
+	else 
+		echo "--------------------------------------------------------------------"
+		echo " About to download and install ufw and its dependencies.  STANDBY!! "
+		echo "--------------------------------------------------------------------"
+		apt update && apt install -y $DEP
+		apt autoremove && apt autoclean
+		echo ""
+		echo " Done.  Now to open the required ports .... "
+fi
+
+elif [ $VERSION = 11 ];
+then
+	echo " Debian 11 (Bullseye) is detected "
+	sleep 2
+	clear
+	if pgrep -x ufw >/dev/null
+	then
+		echo "--------------------------------------------------------------------------"
+		echo " ufw is already installed.  The ports just need to be opened.  Moving on! "
+		echo "--------------------------------------------------------------------------"
+	else
+		echo "--------------------------------------------------------------------"
+		echo " About to download and install ufw and its dependencies.  STANDBY!! "
+		echo "--------------------------------------------------------------------"
+		apt update && apt install -y $DEP
+		apt autoremove && apt autoclean
+		echo ""
+		echo " Done.  Now to open the required ports .... "
+fi
+
+else
+clear
+echo "------------------------------------------------------------------------------------------"
+echo " Operating system not supported! Please check your configuration or upgrade. Exiting .... "
+echo "------------------------------------------------------------------------------------------"
+exit 0
+fi
+
+sleep 3
+echo "------------------------------------------------------------"
+echo " Enabling ufw and opening the proper ports for HBlink3 .... "
+echo "------------------------------------------------------------"
+ufw allow $sshport/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 4321/tcp
+ufw allow 9000/tcp
+ufw allow 62030:62050/udp
+sleep 2
+ufw enable
+sleep 2
+ufw status
+echo ""
+echo "---------------------------------------------------"
+echo " ufw enabled and required ports are opened now and "
+echo "       All other ports are closed.  73             "
+echo "---------------------------------------------------"
